043ee475ef1f1e3a258f124cf76719575248eaf3
minervaDutta <dutta.minerva@gmail.com>
2022-12-23 03:09:19 +0000

    finished project

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import requests
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -36,16 +38,88 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
 
-    return jsonify(response_body), 200
+#     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    response_API = requests.get('https://swapi.dev/api/people')
+    print(response_API)
+
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return response_API.json()["results"], 200
+
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    response_API = requests.get('https://swapi.dev/api/planets')
+    print(response_API)
+
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return response_API.json()["results"], 200
+
+
+@app.route('/people/<people_id>', methods=['GET'])
+def get_all_people_id(people_id):
+    response_API = requests.get('https://swapi.dev/api/people/'+people_id)
+    print(response_API)
+
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return response_API.json(), 200
+
+@app.route('/planets/<planet_id>', methods=['GET'])
+def get_all_planet_id(planet_id):
+    response_API = requests.get('https://swapi.dev/api/planets/'+planet_id)
+    print(response_API)
+
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return response_API.json(), 200
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    # query = db.select([User])
+    # result = connection.execute(query).fetchall()
+    users_query=User.query.all()
+    all_users=list(map(lambda x:x.serialize(),users_query))
+    print(all_users)
+    return all_users, 200
+
+@app.route('/favourite/planet/<planet_id>/<user_id>', methods=['GET'])
+def get_all_users_planets(planet_id, user_id):
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        user1 = User(id=user_id, favourite_planets=json.dumps([planet_id]), favourite_people=json.dumps([]))
+        db.session.add(user1)
+        db.session.commit()
+        return "OK", 200
+    fav_planets = json.loads(user1.favourite_planets) #changes str to list
+    fav_planets.append(planet_id) #adds to list
+    user1.favourite_planets = json.dumps(fav_planets) #list to str
+
+    db.session.commit()
+    return "New planet added", 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+# [GET] /people
--- src/models.py
+++ src/models.py
@@ -1,19 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # email = db.Column(db.String(120), unique=True, nullable=False)
+    # password = db.Column(db.String(80), unique=False, nullable=False)
+    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favourite_planets = db.Column(db.String(200))
+    favourite_people = db.Column(db.String(200))
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            # "email": self.email,
             # do not serialize the password, its a security breach
+            "favourite_planets": json.loads(self.favourite_planets),
+            "favourite_people": json.loads(self.favourite_people)
         }
